5660e1aa073dcf95416f8452bf4ba003e5e5613b
Erin Schnabel <schnabel@us.ibm.com>
2018-09-10 13:55:53 -0400

    remove mhub download from image
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- room-wlpcfg/startup.sh
+++ room-wlpcfg/startup.sh
@@ -51,7 +51,6 @@ if [ "$ETCDCTL_ENDPOINT" != "" ]; then
   #from github, and have to use an extra config snippet to enable it.
   export MESSAGEHUB_USER=$(etcdctl get /kafka/user)
   export MESSAGEHUB_PASSWORD=$(etcdctl get /passwords/kafka)
-  wget https://github.com/ibm-messaging/message-hub-samples/raw/master/java/message-hub-liberty-sample/lib-message-hub/messagehub.login-1.0.0.jar
 fi
 
 if [ -f /etc/cert/cert.pem ]; then
@@ -61,7 +60,7 @@ if [ -f /etc/cert/cert.pem ]; then
   echo "-cd dir"
   cd ${SERVER_PATH}/resources/
   echo "-importing jvm truststore to server truststore"
-  keytool -importkeystore -srckeystore $JAVA_HOME/lib/security/cacerts -destkeystore security/truststore.jks -srcstorepass changeit -deststorepass truststore  
+  keytool -importkeystore -srckeystore $JAVA_HOME/lib/security/cacerts -destkeystore security/truststore.jks -srcstorepass changeit -deststorepass truststore
   echo "-converting pem to pkcs12"
   openssl pkcs12 -passin pass:keystore -passout pass:keystore -export -out cert.pkcs12 -in /etc/cert/cert.pem
   echo "-importing pem to truststore.jks"
